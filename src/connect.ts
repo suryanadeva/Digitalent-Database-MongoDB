115fff1afd64171edf9874d7bb863c8bac4342dc
suryanadeva <suryanadeva2504@gmail.com>
2020-10-24 04:07:53 +0700

    commit all

---
+++ src/connect.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import 'colorts/lib/string';
+
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(`${process.env.MONGODB_URI}`, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    console.log("berhasil connec database")
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
